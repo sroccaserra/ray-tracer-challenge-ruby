9bdf63d4f32534716850417394aecec7c380221a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-04-30 14:54:24 +0200

    Add the notion of point and vector

--- lib/tuple.rb
+++ lib/tuple.rb
@@ -1,9 +1,24 @@
+def tuple(x, y, z, w)
+  if w == 1.0
+    Point.new(x, y, z, w)
+  else
+    Vector.new(x, y, z, w)
+  end
+end
+
 class Tuple
   attr_reader :x, :y, :z, :w
+
   def initialize(x, y, z, w)
     @x = x
     @y = y
     @z = z
     @w = w
   end
+end
+
+class Point < Tuple
+end
+
+class Vector < Tuple
 end
--- spec/tuple_spec.rb
+++ spec/tuple_spec.rb
@@ -2,12 +2,36 @@ require_relative '../lib/tuple.rb'
 
 describe 'Tuples' do
   describe 'A tuple with w=1.0' do
-    it 'should be a point'do 
-      a = Tuple.new(4.3, -4.2, 3.1, 1.0)
+    it 'should have x, y, z, w coordinates' do
+      a = tuple(4.3, -4.2, 3.1, 1.0)
+
       expect(a.x).to eq 4.3
       expect(a.y).to eq(-4.2)
       expect(a.z).to eq 3.1
       expect(a.w).to eq 1.0
+    end
+
+    it 'should be a point'do
+      a = tuple(4.3, -4.2, 3.1, 1.0)
+
+      expect(a).to be_a Point
+    end
+  end
+
+  describe 'A tuple with w=0' do
+    it 'should have x, y, z, w coordinates' do
+      a = tuple(4.3, -4.2, 3.1, 0)
+
+      expect(a.x).to eq 4.3
+      expect(a.y).to eq(-4.2)
+      expect(a.z).to eq 3.1
+      expect(a.w).to eq 0
+    end
+
+    it 'should be a vector'do
+      a = tuple(4.3, -4.2, 3.1, 0)
+
+      expect(a).to be_a Vector
     end
   end
 end
