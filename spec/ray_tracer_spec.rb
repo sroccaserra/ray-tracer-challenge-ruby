8c9da1bcab23d4f05194eb33c23d94f927d2de0b
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-04-30 23:10:29 +0200

    Organize tests better

--- spec/ray_tracer_spec.rb
+++ spec/ray_tracer_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/ray_tracer.rb'
 
-describe '#tuple' do
+describe 'Tuple' do
   describe 'A tuple with w=1.0' do
     it 'should have x, y, z, w coordinates' do
       a = tuple(4.3, -4.2, 3.1, 1.0)
@@ -34,65 +34,67 @@ describe '#tuple' do
       expect(a).to be_a RayTracer::Vector
     end
   end
-end
 
-describe '#point' do
-  it 'should create tuples with w=1' do
-    p = point(4, -4, 3)
+  describe '#point' do
+    it 'should create tuples with w=1' do
+      p = point(4, -4, 3)
 
-    expect(p).to eq tuple(4, -4, 3, 1)
-  end
-end
-
-describe '#vector' do
-  it 'should create tuples with w=0' do
-    p = vector(4, -4, 3)
-
-    expect(p).to eq tuple(4, -4, 3, 0)
-  end
-end
-
-describe 'adding tuples' do
-  it 'should add a point and a vector' do
-    a = point(3, -2, 5)
-    b = vector(-2, 3, 1)
-
-    expect(a + b).to eq point(1, 1, 6)
+      expect(p).to eq tuple(4, -4, 3, 1)
+    end
   end
 
-  it 'should add two vectors' do
-    v1 = vector(3, -2, 5)
-    v2 = vector(-2, 3, 1)
+  describe '#vector' do
+    it 'should create tuples with w=0' do
+      p = vector(4, -4, 3)
 
-    expect(v1 + v2).to eq vector(1, 1, 6)
+      expect(p).to eq tuple(4, -4, 3, 0)
+    end
   end
 
-  it 'should substract two points' do
-    p1 = point(3, 2, 1)
-    p2 = point(5, 6, 7)
+  describe '#+' do
+    it 'should add a point and a vector' do
+      a = point(3, -2, 5)
+      b = vector(-2, 3, 1)
 
-    expect(p1 - p2).to eq vector(-2, -4, -6)
+      expect(a + b).to eq point(1, 1, 6)
+    end
+
+    it 'should add two vectors' do
+      v1 = vector(3, -2, 5)
+      v2 = vector(-2, 3, 1)
+
+      expect(v1 + v2).to eq vector(1, 1, 6)
+    end
   end
 
-  it 'should supstract a vector from a point' do
-    p = point(3, 2, 1)
-    v = vector(5, 6, 7)
+  describe '#-' do
+    it 'should substract two points' do
+      p1 = point(3, 2, 1)
+      p2 = point(5, 6, 7)
 
-    expect(p - v).to eq point(-2, -4, -6)
-  end
+      expect(p1 - p2).to eq vector(-2, -4, -6)
+    end
 
-  it 'should substract two vectors' do
-    v1 = vector(3, 2, 1)
-    v2 = vector(5, 6, 7)
+    it 'should substract a vector from a point' do
+      p = point(3, 2, 1)
+      v = vector(5, 6, 7)
 
-    expect(v1 - v2).to eq vector(-2, -4, -6)
-  end
+      expect(p - v).to eq point(-2, -4, -6)
+    end
 
-  it 'should substract from the zero vector' do
-    zero = vector(0, 0, 0)
-    v = vector(1, -2, 3)
+    it 'should substract two vectors' do
+      v1 = vector(3, 2, 1)
+      v2 = vector(5, 6, 7)
 
-    expect(zero - v).to eq vector(-1, 2, -3)
+      expect(v1 - v2).to eq vector(-2, -4, -6)
+    end
+
+    it 'should substract from the zero vector' do
+      zero = vector(0, 0, 0)
+      v = vector(1, -2, 3)
+
+      expect(zero - v).to eq vector(-1, 2, -3)
+    end
   end
 
   describe '#negate' do
