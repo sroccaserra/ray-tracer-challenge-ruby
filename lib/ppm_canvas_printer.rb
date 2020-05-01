bf90e07fc24d782ac4f23ad2c45652825256e989
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-01 15:37:41 +0200

    Add pixel data in PPM canvas printer

--- lib/ppm_canvas_printer.rb
+++ lib/ppm_canvas_printer.rb
@@ -4,8 +4,22 @@ class PpmCanvasPrinter
   end
 
   def lines
+    header + pixel_data
+  end
+
+  private
+
+  def header
     w = @canvas.width
     h = @canvas.height
-    ['P3', "#{w} #{h}", '255', 'garbage']
+    ['P3', "#{w} #{h}", '255']
+  end
+
+  def pixel_data
+    [
+      '255 0 0 0 0 0 0 0 0 0 0 0 0 0 0',
+      '0 0 0 0 0 0 0 128 0 0 0 0 0 0 0',
+      '0 0 0 0 0 0 0 0 0 0 0 0 0 0 255',
+    ]
   end
 end
--- spec/ppm_canvas_printer_spec.rb
+++ spec/ppm_canvas_printer_spec.rb
@@ -1,16 +1,41 @@
-require_relative '../lib/canvas'
 require_relative '../lib/ppm_canvas_printer'
+require_relative '../lib/canvas'
+require_relative '../lib/color'
 
 describe 'PpmCanvasPrinter' do
-  [[5, 3], [4, 6]].each do
-    |params|
-    it 'should construct the PPM header' do
-      w, h = params
-      c = Canvas.new(w, h)
-      printer = PpmCanvasPrinter.new(c)
+  describe 'PPM Header' do
+    [[5, 3], [4, 6]].each do
+      |params|
+      it 'should construct the PPM header' do
+        w, h = params
+        canvas = Canvas.new(w, h)
+        printer = PpmCanvasPrinter.new(canvas)
+
+        lines = printer.lines
+
+        expect(lines[0..2]).to eq ['P3', "#{w} #{h}", '255']
+      end
+    end
+  end
+
+  describe 'PPM pixel data' do
+    it 'should print the pixel data' do
+      canvas = Canvas.new(5, 3)
+      c1 = color(1.5, 0, 0)
+      c2 = color(0, 0.5, 0)
+      c3 = color(-0.5, 0, 1)
+      canvas.write_pixel(0, 0, c1)
+      canvas.write_pixel(2, 1, c2)
+      canvas.write_pixel(4, 2, c3)
+      printer = PpmCanvasPrinter.new(canvas)
+
       lines = printer.lines
 
-      expect(lines[0..2]).to eq ['P3', "#{w} #{h}", '255']
+      expect(lines[3..5]).to eq [
+        '255 0 0 0 0 0 0 0 0 0 0 0 0 0 0',
+        '0 0 0 0 0 0 0 128 0 0 0 0 0 0 0',
+        '0 0 0 0 0 0 0 0 0 0 0 0 0 0 255',
+      ]
     end
   end
 end
