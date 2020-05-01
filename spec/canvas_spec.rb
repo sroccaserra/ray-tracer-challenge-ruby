2935801701394bc44f3fbc7a48134c3aa5cfde36
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-01 13:29:00 +0200

    r Extract before each hook in canvas tests

--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -2,30 +2,32 @@ require_relative '../lib/canvas'
 require_relative '../lib/color'
 
 describe 'Canvas' do
-  it 'should be blank at creation' do
-    c = Canvas.new(10, 20)
-    expect(c.width).to be 10
-    expect(c.height).to be 20
+  before(:each) do
+    @c = Canvas.new(10, 20)
+  end
 
-    expect(c.is_blank?).to be true
+  it 'should be blank at creation' do
+    expect(@c.width).to be 10
+    expect(@c.height).to be 20
+    expect(@c.is_blank?).to be true
   end
 
   it 'should allow writing pixels' do
-    c = Canvas.new(10, 20)
     red = color(1, 0, 0)
-    c.write_pixel(2, 3, red)
 
-    expect(c.is_blank?).to be false
+    @c.write_pixel(2, 3, red)
+
+    expect(@c.is_blank?).to be false
   end
 
   it 'should allow reading pixels' do
-    c = Canvas.new(10, 20)
     red = color(1, 0, 0)
     blue = color(0, 0, 1)
-    c.write_pixel(2, 3, red)
-    c.write_pixel(3, 4, blue)
 
-    expect(c.pixel_at(2, 3)).to eq red
-    expect(c.pixel_at(3, 4)).to eq blue
+    @c.write_pixel(2, 3, red)
+    @c.write_pixel(3, 4, blue)
+
+    expect(@c.pixel_at(2, 3)).to eq red
+    expect(@c.pixel_at(3, 4)).to eq blue
   end
 end
