a72c2b38c2479685e1409b5d044e34711109137f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-03 01:16:03 +0200

    r Remove duplcation in chain transformation checks

--- spec/transformation_chain_spec.rb
+++ spec/transformation_chain_spec.rb
@@ -4,15 +4,18 @@ require_relative '../lib/scaling'
 require_relative '../lib/translation'
 
 describe 'Chaining transformations' do
+  before(:each) do
+    @a = rotation_x(Math::PI/2)
+    @b = scaling(5, 5, 5)
+    @c = translation(10, 5, 7)
+  end
+
   it 'applies in sequence' do
     p1 = point(1, 0, 1)
-    a = rotation_x(Math::PI/2)
-    b = scaling(5, 5, 5)
-    c = translation(10, 5, 7)
 
-    p2 = a*p1
-    p3 = b*p2
-    p4 = c*p3
+    p2 = @a*p1
+    p3 = @b*p2
+    p4 = @c*p3
 
     expect(p2).to eq_epsilon point(1, -1, 0)
     expect(p3).to eq_epsilon point(5, -5, 0)
@@ -21,10 +24,7 @@ describe 'Chaining transformations' do
 
   it 'multiplies in reverse order' do
     p = point(1, 0, 1)
-    a = rotation_x(Math::PI/2)
-    b = scaling(5, 5, 5)
-    c = translation(10, 5, 7)
-    transformation = c*b*a
+    transformation = @c*@b*@a
 
     expect(transformation*p).to eq_epsilon point(15, 0, 7)
   end
