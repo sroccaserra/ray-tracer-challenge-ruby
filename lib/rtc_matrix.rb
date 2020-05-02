4d3020ab01281d6e0fa11dc3f5ad25dd54ec7bd4
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-02 15:07:35 +0200

    r Wrap Ruby Matrix

---
+++ lib/rtc_matrix.rb
@@ -0,0 +1,22 @@
+require 'matrix'
+require_relative './tuple'
+
+def matrix(*rows)
+  RayTracerChallenge::Matrix.new(::Matrix.rows(rows))
+end
+
+module RayTracerChallenge
+  Matrix = Struct.new(:matrix) do
+    def [](i, j)
+      matrix[i, j]
+    end
+
+    def *(other)
+      if other.is_a?(Tuple)
+        Tuple.new(matrix*other.vector)
+      else
+        Matrix.new(matrix*other.matrix)
+      end
+    end
+  end
+end
--- spec/rtc_matrix_spec.rb
+++ spec/rtc_matrix_spec.rb
@@ -1,15 +1,14 @@
-require 'matrix'
-
+require_relative '../lib/rtc_matrix'
 require_relative '../lib/tuple'
 
-describe 'Matrix' do
+describe 'RayTracerChallenge::Matrix' do
   it 'should be queryable' do
-    m = Matrix[
+    m = matrix(
       [1, 2, 3, 4],
       [5.5, 6.5, 7.5, 8.5],
       [9, 10, 11, 12],
       [13.5, 14.5, 15.5, 16.5],
-    ]
+    )
 
     expect(m[0, 0]).to be 1
     expect(m[0, 3]).to be 4
@@ -21,10 +20,10 @@ describe 'Matrix' do
   end
 
   it 'should allow 2x2 dimensions' do
-    m = Matrix[
+    m = matrix(
       [-3, 5],
       [1 , -2],
-    ]
+    )
 
     expect(m[0,0]).to be(-3)
     expect(m[0,1]).to be 5
@@ -33,11 +32,11 @@ describe 'Matrix' do
   end
 
   it 'should allow 3x3 dimensions' do
-    m = Matrix[
+    m = matrix(
       [-3, 5, 0],
       [1 , -2, -7],
       [0 , 1, 1],
-    ]
+    )
 
     expect(m[0,0]).to be(-3)
     expect(m[1,1]).to be(-2)
@@ -46,35 +45,35 @@ describe 'Matrix' do
 
   describe 'matrix comparison' do
     it 'should be equal' do
-      a = Matrix[
+      a = matrix(
         [1, 2, 3, 4],
         [5, 6, 7, 8],
         [9, 8, 7, 6],
         [5, 4, 3, 2],
-      ]
-      b = Matrix[
+      )
+      b = matrix(
         [1, 2, 3, 4],
         [5, 6, 7, 8],
         [9, 8, 7, 6],
         [5, 4, 3, 2],
-      ]
+      )
 
       expect(a).to eq b
     end
 
     it 'should be different' do
-      a = Matrix[
+      a = matrix(
         [1, 2, 3, 4],
         [5, 6, 7, 8],
         [9, 8, 7, 6],
         [5, 4, 3, 2],
-      ]
-      b = Matrix[
+      )
+      b = matrix(
         [2, 3, 4, 5],
         [6, 7, 8, 9],
         [8, 7, 6, 5],
         [4, 3, 2, 1],
-      ]
+      )
 
       expect(a).not_to eq b
     end
@@ -82,37 +81,37 @@ describe 'Matrix' do
 
   describe 'Multiplication' do
     it 'should multiply by another matrix' do
-      a = Matrix[
+      a = matrix(
         [1, 2, 3, 4],
         [5, 6, 7, 8],
         [9, 8, 7, 6],
         [5, 4, 3, 2],
-      ]
-      b = Matrix[
+      )
+      b = matrix(
         [-2, 1, 2, 3],
         [3, 2, 1, -1],
         [4, 3, 6, 5],
         [1, 2, 7, 8],
-      ]
+      )
 
-      expect(a*b).to eq Matrix[
+      expect(a*b).to eq matrix(
         [20, 22, 50, 48],
         [44, 54, 114, 108],
         [40, 58, 110, 102],
         [16, 26, 46, 42],
-      ]
+      )
     end
 
     it 'should multiply by a tuple' do
-      a = Matrix[
+      a = matrix(
         [1, 2, 3, 4],
         [2, 4, 4, 2],
         [8, 6, 4, 1],
         [0, 0, 0, 1],
-      ]
+      )
       b = tuple(1, 2, 3, 1)
 
-      expect(a*b.vector).to eq tuple(18, 24, 33, 1).vector
+      expect(a*b).to eq tuple(18, 24, 33, 1)
     end
   end
 end
