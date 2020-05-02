f99980290bd106ea3d5238ce7b90fa5057cfad25
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-03 01:11:43 +0200

    Check chains of transformations

---
+++ spec/transformation_chain_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/rotation'
+require_relative '../lib/rtc_tuple'
+require_relative '../lib/scaling'
+require_relative '../lib/translation'
+
+describe 'Chaining transformations' do
+  it 'applies in sequence' do
+    p1 = point(1, 0, 1)
+    a = rotation_x(Math::PI/2)
+    b = scaling(5, 5, 5)
+    c = translation(10, 5, 7)
+
+    p2 = a*p1
+    p3 = b*p2
+    p4 = c*p3
+
+    expect(p2).to eq_epsilon point(1, -1, 0)
+    expect(p3).to eq_epsilon point(5, -5, 0)
+    expect(p4).to eq_epsilon point(15, 0, 7)
+  end
+
+  it 'multiplies in reverse order' do
+    p = point(1, 0, 1)
+    a = rotation_x(Math::PI/2)
+    b = scaling(5, 5, 5)
+    c = translation(10, 5, 7)
+    transformation = c*b*a
+
+    expect(transformation*p).to eq_epsilon point(15, 0, 7)
+  end
+end
