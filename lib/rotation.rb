67eb2be52420801d119baaa107a16f8f9df7fc50
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-03 00:41:52 +0200

    Add y and z axis rotations

--- lib/rotation.rb
+++ lib/rotation.rb
@@ -8,3 +8,21 @@ def rotation_x(r)
     [0, 0, 0, 1],
   )
 end
+
+def rotation_y(r)
+  matrix(
+    [Math.cos(r), 0, Math.sin(r), 0],
+    [0, 1, 0, 0],
+    [-Math.sin(r), 0, Math.cos(r), 0],
+    [0, 0, 0, 1],
+  )
+end
+
+def rotation_z(r)
+  matrix(
+    [Math.cos(r), -Math.sin(r), 0, 0],
+    [Math.sin(r), Math.cos(r), 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+  )
+end
--- spec/rotation_spec.rb
+++ spec/rotation_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/rtc_tuple'
 require_relative '../lib/rotation'
 
 describe 'Rotations' do
-  describe 'Along the x axis' do
+  describe 'Around the x axis' do
     it 'should allow rotation' do
       p = point(0, 1, 0)
       half_quarter = rotation_x(Math::PI/4)
@@ -17,6 +17,30 @@ describe 'Rotations' do
       half_quarter = rotation_x(Math::PI/4)
 
       expect(half_quarter.inverse*p).to eq_epsilon point(0, Math.sqrt(2)/2, -Math.sqrt(2)/2)
+    end
+  end
+
+  describe 'Around the y axis' do
+    it 'should allow rotation' do
+      p = point(0, 0, 1)
+
+      half_quarter = rotation_y(Math::PI/4)
+      full_quarter = rotation_y(Math::PI/2)
+
+      expect(half_quarter*p).to eq_epsilon point(Math.sqrt(2)/2, 0, Math.sqrt(2)/2)
+      expect(full_quarter*p).to eq_epsilon point(1, 0, 0)
+    end
+  end
+
+  describe 'Around the z axis' do
+    it 'should allow rotation' do
+      p = point(0, 1, 0)
+
+      half_quarter = rotation_z(Math::PI/4)
+      full_quarter = rotation_z(Math::PI/2)
+
+      expect(half_quarter*p).to eq_epsilon point(-Math.sqrt(2)/2, Math.sqrt(2)/2, 0)
+      expect(full_quarter*p).to eq_epsilon point(-1, 0, 0)
     end
   end
 end
