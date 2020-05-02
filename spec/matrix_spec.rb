4d9754c19e237721536388f9ee2e2b72398f3576
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-02 11:45:01 +0200

    Add matrix by matrix multiplication

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -77,4 +77,28 @@ describe 'Matrix' do
       expect(a).not_to eq b
     end
   end
+
+  describe 'Multiplication' do
+    it 'should multiply by another matrix' do
+      a = Matrix[
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 8, 7, 6],
+        [5, 4, 3, 2],
+      ]
+      b = Matrix[
+        [-2, 1, 2, 3],
+        [3, 2, 1, -1],
+        [4, 3, 6, 5],
+        [1, 2, 7, 8],
+      ]
+
+      expect(a*b).to eq Matrix[
+        [20, 22, 50, 48],
+        [44, 54, 114, 108],
+        [40, 58, 110, 102],
+        [16, 26, 46, 42],
+      ]
+    end
+  end
 end
