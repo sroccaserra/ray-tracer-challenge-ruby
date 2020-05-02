63160a830d076b16c8730292b9c719fa0a8e3d7e
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-03 00:27:57 +0200

    r Make eq_epsilon work with colors

--- spec/rtc_color_spec.rb
+++ spec/rtc_color_spec.rb
@@ -20,11 +20,7 @@ describe 'Color' do
     c1 = color(0.9, 0.6, 0.75)
     c2 = color(0.7, 0.1, 0.25)
 
-    c3 = c1 - c2
-
-    expect(c3.red).to be_within(EPSILON).of 0.2
-    expect(c3.green).to eq 0.5
-    expect(c3.blue).to eq 0.5
+    expect(c1 - c2).to eq_epsilon color(0.2, 0.5, 0.5)
   end
 
   it 'should multiply by a scalar' do
@@ -37,9 +33,6 @@ describe 'Color' do
     c1 = color(1, 0.2, 0.4)
     c2 = color(0.9, 1, 0.1)
 
-    c3 = c1.mul(c2)
-    expect(c3.red).to eq 0.9
-    expect(c3.green).to eq 0.2
-    expect(c3.blue).to be_within(EPSILON).of 0.04
+    expect(c1.mul(c2)).to eq_epsilon color(0.9, 0.2, 0.04)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,16 @@ EPSILON = 0.00001
 
 RSpec::Matchers.define :eq_epsilon do |expected|
   match do |actual|
-    (actual - expected).magnitude < EPSILON
+    actual.values.zip(expected.values).all? do
+      |a, e|
+      are_all_values_within_epsilon?(a, e)
+    end
+  end
+end
+
+def are_all_values_within_epsilon?(actual_values, expected_values)
+  actual_values.zip(expected_values).all? do
+    |a, e|
+    (a - e).abs < EPSILON
   end
 end
