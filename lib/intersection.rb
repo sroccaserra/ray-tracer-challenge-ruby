94bfca6c02ebb3d6a5fefc2e277ce5e8e21b2890
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-05-03 23:57:26 +0200

    Add hit detection

--- lib/intersection.rb
+++ lib/intersection.rb
@@ -1,1 +1,7 @@
 Intersection = Struct.new(:t, :object)
+
+def hit(intersections)
+  intersections
+    .filter { |i| i.t >= 0 }
+    .min_by(&:t)
+end
---
+++ spec/intersection_spec.rb
@@ -0,0 +1,39 @@
+require 'intersection'
+require 'math/tuple'
+require 'sphere'
+
+describe 'Intersection' do
+  before(:each) do
+    @sphere = Sphere.new(point(0, 0, 0), 1)
+  end
+
+  it 'finds the hit when all intersections have positive t' do
+    i1 = Intersection.new(1, @sphere)
+    i2 = Intersection.new(2, @sphere)
+
+    expect(hit([i1, i2])).to be i1
+  end
+
+  it 'finds the hit when some intersections have negative t' do
+    i1 = Intersection.new(-1, @sphere)
+    i2 = Intersection.new(1, @sphere)
+
+    expect(hit([i1, i2])).to be i2
+  end
+
+  it 'finds the hit when all intersections have negative t' do
+    i1 = Intersection.new(-2, @sphere)
+    i2 = Intersection.new(-1, @sphere)
+
+    expect(hit([i1, i2])).to be_nil
+  end
+
+  it 'finds the hit' do
+    i1 = Intersection.new(5, @sphere)
+    i2 = Intersection.new(7, @sphere)
+    i3 = Intersection.new(-3, @sphere)
+    i4 = Intersection.new(2, @sphere)
+
+    expect(hit([i1, i2, i3, i4])).to be i4
+  end
+end
